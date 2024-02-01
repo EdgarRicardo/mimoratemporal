0436c2e9913325570c5a898f1068a5bbc0cd01ad
Edgar Corona <coronam.edgar@gmail.com>
2024-01-31 20:25:54 -0600

    Cambios rama y proyecto build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "https://<usrname>.github.io/<projectname>"
+  base: "https://EdgarRicardo.github.io/miamoratemporal"
 })
