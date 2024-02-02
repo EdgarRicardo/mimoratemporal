ef4a22da34aa4f6b22fc68d155e9d9320c5553b5
Edgar Corona <coronam.edgar@gmail.com>
2024-02-01 21:36:26 -0600

    Prueba fotos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import './css/App.css'
+import PhotoAlbum from 'react-photo-album'
+import { imgsData } from './utils/variables';
+
 const App = () => {
+  const photos = imgsData.map(e => {
+    return { src: "http://drive.google.com/uc?export=view&id=" + e.id, width: 800, height: 600 }
+  });
   return (
     <>
       <h1>MI AMOR ATEMPORAL ❤️</h1>
+      {<PhotoAlbum layout="rows" photos={photos} />}
+      {imgsData.map(e => {
+        return <img loading="lazy" src={"http://drive.google.com/uc?export=view&id="+e.id} key={e.id}></img>
+      })}
     </>
   )
 }
---
+++ src/utils/variables.tsx
@@ -0,0 +1,602 @@
+export const imgsData = [
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pPUVU_u9rhTDY7_elyy5zIlrLxlrHXBF",
+        "name": "IMG20230401221511_20240201145557.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pPJ_oUCVXQJPfe1Pf1CbOYboGS7l_1cw",
+        "name": "IMG20231027182510_20240201145557.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pNSOxTJFNJMOjJsxlfxsJSE9eB_9T_Em",
+        "name": "IMG20231028195828_20240201145557.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pI1Bif4b8TdVatRHJGpSYAxweaoptHV_",
+        "name": "IMG20231028201936_20240201145557.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pH7tKeLowXIuCnmCjXnoI4FLYVw5UQNR",
+        "name": "IMG20231028202006_20240201145557.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pG4y805Nj2hYnuud80kfzYWo8j_RBLnf",
+        "name": "IMG20231028202601_20240201145557.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pFXCOOYVMzoWAam0783zCTasLM7viHgZ",
+        "name": "IMG20231209190649_20240201145556.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1pCSWZrbZqCeViQiKKqjsils6EWfAnMWp",
+        "name": "IMG20231209190723_20240201145556.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1p9NFP5yeaByjokN4DiP6D2CGGpWUKKBR",
+        "name": "IMG20231209192945_20240201145556.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1p0t211jn7hZjgPs_FTs5ZFjJtECP4h6L",
+        "name": "IMG20231209204644_20240201145556.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1p0EdK_LY7-i9nifBQDeXyBgdQaDuZhUs",
+        "name": "IMG20231209233452_20240201145556.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1p-DT0U4Aeq05M7b2kL3YezLTjEGUQ4Q5",
+        "name": "IMG20240101003431_20240201145556.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oreSfjbLADDmawSoN-SoiOw267nS7QHv",
+        "name": "IMG20240120185500_20240201145555.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oqMPJhoqMuljlltOxjyizLUfZUllifq_",
+        "name": "IMG20240120185503_20240201145555.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oo3CM_zlUzZtx8OAQnyO1WD6LCA2gyf8",
+        "name": "IMG20240128124120_20240201145554.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1okrHvoL0NBhn-_5N6JBX3KngjNlwTbxe",
+        "name": "IMG20240128131442_20240201145554.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1ohlbHh-97IRQoOXBbnnKHDhUVQOgudq6",
+        "name": "IMG20240128131510_20240201145554.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1ogsXN6_YNsmNz5-Wjd0v17Bs0OSFqIGX",
+        "name": "IMG20240128132128_20240201145554.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oewvGVkuaNCUu8Pi9juR0htYi71aGiko",
+        "name": "IMG20240128142148_20240201145553.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1obds8fQerCJrEGNev3Jee55gZjCHJ7WV",
+        "name": "IMG20240128145959_20240201145553.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oOedWhALLctyoVML-jK8WOhyqCm10zLT",
+        "name": "IMG20240128150220_20240201145553.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oMxe-wVznv8XcU9ysCSGKb-8-SHR965H",
+        "name": "IMG20240128150223_20240201145553.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oGKyucV2U-2ccD5Ex7ZRLazZShyn2uVz",
+        "name": "IMG20230915181752_20240201145553.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oEFaiaeG7MeL0azPKUbLyvv0TdXS4qUz",
+        "name": "IMG20230915181815_20240201145552.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oE6_nMz6hi_IGVeoYowDVQ4MWnPLZBFn",
+        "name": "IMG20230916131201_20240201145552.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1oDu6DVBZzKY7iKS6lWWYJ2LpcbFVPpeq",
+        "name": "IMG20230916131221_20240201145552.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1o8Brjhk8nMHFVBMAdbmTw3mDmi6-PsSu",
+        "name": "IMG20230812152625_20240201145552.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1o4vDGGNxVY0ZQEWCS4k0ozauc7_st3jk",
+        "name": "IMG20230812152628_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1o4noNTFadKNgUaj84LzMHRMpvBg_uzQ4",
+        "name": "IMG20230909170413_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1o4MY5BY5QKxfva3d97YacxQY44S0YwPI",
+        "name": "IMG20230909194634_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1o11itYRbKmxHr73ZZEhzqeEA67kMkODR",
+        "name": "IMG-20230704-WA0012.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1o-CSc1c2qYsWRcaYYwKugkPXLE5-F5bN",
+        "name": "IMG20230722204331_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nuE57Lh2j00Qjx7_aBsmKKw_CO4scwN_",
+        "name": "IMG20230722204334_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1ntYPaOL27CO-L4nnKZ_ev94RYa3UvJwN",
+        "name": "IMG20230812142411_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nomuAX8h89cX3PnrXLappZYx7-oiMK9c",
+        "name": "IMG20230701103339_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nnmZIXqt8PYUKY8R-hKWmsc3houhrrjY",
+        "name": "IMG20230701120752_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nh0r_U6RRDf_THSzkh1Utyt-2AIbyvev",
+        "name": "IMG20230701121901_20240201145551.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nZt1gxWf2Rp2zc6xHzC9KsJswhFu4qOv",
+        "name": "IMG20230701121909_20240201145550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nRT8AfEypfgBc-5mZtScaOOMWE6mWPBR",
+        "name": "IMG20230629120004.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nQmr8zrgd3gO-fCsBfMEBTMEurzkAp9a",
+        "name": "IMG_20230629_182734.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nGbuVIn75_h42jXygWdjL9ZjcGWc3X1f",
+        "name": "IMG20230630111904.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nGNbmBmW3TTSFqS0J0OlzgaYuFXNMg2_",
+        "name": "IMG20230630122753.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1nA7va90C-nbx5LTMX7Ka23bVOAxasXg0",
+        "name": "IMG20230628095545_20240201145550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mvdDmaC1Fgox1_I2N9czsm0LCwdD4FBq",
+        "name": "IMG20230628180508_20240201145550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mtOb9lVw-7FGMfjelLFZcBMUjFpbtl9O",
+        "name": "IMG20230628180532_20240201145550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1msXguL73IYV-nPY2mKogOfwp5nxRaVrC",
+        "name": "IMG20230628183635_20240201145550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mrNuZdY1sZa2pMvIZtpxO5qrghC60XKH",
+        "name": "IMG20230506105719_20240201145550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mmBg7PZJDDYlkSAVR1453kckplcsoSa3",
+        "name": "IMG20230506105720_20240201145549.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1meWleBYp2CMulLdR6ER3rMBu5BysRVKQ",
+        "name": "IMG20230610181741_20240201145549.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1meIKk3Zf9fUDgpBoe66eh_Upm4LgLFH6",
+        "name": "IMG20230627141033.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mdzRhUqcSQhkA_kgvkfrPXhRwpTmU2Xq",
+        "name": "IMG20230506105718_20240201145549.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mcvGC6a5WnYp3qMb8wRoR5wd6x70jOlc",
+        "name": "IMG20230408143634_20240201145549.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mceab015bJkBkT6U9i_ux5eeU4mPcp70",
+        "name": "IMG20230408142935_20240201145549.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mO-sNNVuil_nf4uD3gMuVQ6rj0tEx_FR",
+        "name": "IMG20230408123537.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mLvMkhewfriyVqnrpyjcTSULcRBOgPCj",
+        "name": "IMG20230407162940.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mLL73acoCyi1doASfSF6uXfc9w1KAYy1",
+        "name": "IMG20230407160652_20240201145549.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mIfAVYN0jMmpZ9of_ogLUTgT8TuHE2Wd",
+        "name": "IMG20230401223842_20240201145549.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1mDIwqMjHYZ51BVPvo9Pw4dfH_b-6wEDA",
+        "name": "IMG20230224155550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1m0Cq0e68GesR0DfDC96YAWG3zmVoNFke",
+        "name": "IMG20230227200412_20240201145547.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1lsKsbx5LIuRlDXZ3cjnywCLrcynmz2Pv",
+        "name": "IMG20230311141951.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1lfHb265h46nzKFeDpWCWaZOve3F5MAjD",
+        "name": "IMG20230311161028_20240201145547.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1leHeAeLqflyGmMcIBRBGUmPEtV0VRx9g",
+        "name": "IMG20230401221310.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1l_tlPT3iOyJ2P-WTTtpQo6u4wIJstnAL",
+        "name": "IMG20230129132850_20240201145547.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1lUDa9jUrVfOw7ErAHh9jkT3uVb6gNVXo",
+        "name": "IMG20230129151906_20240201145547.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1lDnNkxFNtM8Dj2bQBg-CFL7w-FlZXWWm",
+        "name": "IMG20230224155415.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1l-3HmlKnuLlLyaA1nQye5Rn8kVgXtU7A",
+        "name": "IMG_20221229_235939.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kvQqypliEpQpje9_loHEiWk03qInkkdg",
+        "name": "IMG_20230118_195554_868.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kreeTKTbE3xiRnV96D9eIPuXPbN80GeX",
+        "name": "IMG20230128192508_20240201145547.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kqXPLnxKCYDOV9EDvdDve5raxXCtucgB",
+        "name": "IMG20230128192546_20240201145547.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kpYBHh9N9ANReRY4J5_I055GIkkc_z4R",
+        "name": "IMG20221223221014.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kpFAPVaKlRJb2FNeNdGXb1JmQ1dobzz5",
+        "name": "IMG_20221225_232741.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kmRgi8euz5PTW5hJ45FtE7U4NV30Cyj-",
+        "name": "IMG_20221222_230452.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "application/vnd.google-apps.folder",
+        "id": "1P_G0yCyDOZdn9pxiVQff0Rff7gFzcNmV",
+        "name": "Mipreciosa&Yo"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kmNWVnjhPoa6agLrmBmvpP9LFuqF91Rt",
+        "name": "IMG_20221222_230452.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kdK4wfhArzYaS-E5XlokIRCP72mCfIeZ",
+        "name": "IMG20221223221014.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kbzDruJQXUi-4GqrG7XEEwFvPqswsUG5",
+        "name": "IMG_20221225_232741.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kSHkM0R8ZmsKR5VwoM50NY24mG6hETZM",
+        "name": "IMG_20221229_235939.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1kPtDDjECegMbqPNfdoUM8KFSKsdJxntz",
+        "name": "IMG_20230118_195554_868.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1kPfjkclnTExhZBcZvaX_sZOEHXRLoZ_U",
+        "name": "IMG20230128192508.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1kItYSQFagFJ2EOzsYDGJnVw9avBshgiT",
+        "name": "IMG20230128192546.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1kHn-4dGXgNz3vT-4bOyhfv-Uwodsr3y7",
+        "name": "IMG20230129132850.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1kGD8Kqq2WFUsBhrEBLH_elb26Dge5sVP",
+        "name": "IMG20230129151906.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1k9sZa50fP-ExZAqiyJ9hi0Si-WCwOo-2",
+        "name": "IMG20230224155415.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1k5z5o_DiAJAioNrSUrR4Ii9EjX73G2JL",
+        "name": "IMG20230224155550.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1k3zCLdYJIQOqKz0AYLjfatggzVLkPRYo",
+        "name": "IMG20230227200412.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1k0bPOR8bQkRHV8QaaPcYkCzz_5jiK6gY",
+        "name": "IMG20230311141951.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1jxYMN0nY5khJkuleDE-9URqtyw41KODy",
+        "name": "IMG20230311161028.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1jhi3RxXpE-pXj3PdQdFArX79QSNLke2t",
+        "name": "IMG20230401221310.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1jYfzdglDL8MVcdqggta6Sp_clDuXjg23",
+        "name": "IMG20230401221511.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1jYEXy9LJc3IeivPziL3lzvuCIG4aAF09",
+        "name": "IMG20230401223842.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1jV-SDtmWG9sydlV5XBR1Rk9pZZgXA-ut",
+        "name": "IMG20230407160652.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1jT0P0VzoUdA4YXvOgYXHa94GlXMQ50js",
+        "name": "IMG20230407162940.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1jSYBPnbqn_1iGJ3UyLtcttpxYSPvpnrj",
+        "name": "IMG20230408123537.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1jODUJ-0_6NBw9bqEjw3UI6kJD0RvqoeY",
+        "name": "IMG20230408142935.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1jEkSxHRJaZljXWTufBrG5jEJd1EPGiC1",
+        "name": "IMG20230408143634.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1j73gmjBHXrV6iwLeIMZAmfQNQvIicA7m",
+        "name": "IMG20230506105718.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/jpeg",
+        "id": "1j24weH1yqTbPaQRtbb0hDtclFbkhKgwk",
+        "name": "IMG20230627141033.jpg"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1j-qW3ptvenhhfwGSKgl3PzkjHt5D2Iwd",
+        "name": "IMG20230628183635.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1iz8tDjO9JnxKj1DH2sVQeX2TksOp9S19",
+        "name": "IMG20230506105719.heic"
+    },
+    {
+        "kind": "drive#file",
+        "mimeType": "image/heif",
+        "id": "1is5myZc9BUgRt3SiPJefavOQXVBPyaYs",
+        "name": "IMG20230506105720.heic"
+    }
+]
