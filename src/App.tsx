d5052f6028cdd63aa195b68fad831a5da1491718
Edgar Corona <coronam.edgar@gmail.com>
2024-02-01 21:46:11 -0600

    Prueba fotos 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { imgsData } from './utils/variables';
 
 const App = () => {
   const photos = imgsData.map(e => {
-    return { src: "http://drive.google.com/uc?export=view&id=" + e.id, width: 800, height: 600 }
+    return { src: `https://drive.usercontent.google.com/download?id=${e.id}&export=view&authuser=0`, width: 800, height: 600 }
   });
   return (
     <>
       <h1>MI AMOR ATEMPORAL ❤️</h1>
       {<PhotoAlbum layout="rows" photos={photos} />}
       {imgsData.map(e => {
-        return <img loading="lazy" src={"http://drive.google.com/uc?export=view&id="+e.id} key={e.id}></img>
+        return <img loading="lazy" src={`https://drive.usercontent.google.com/download?id=${e.id}&export=view&authuser=0`} key={e.id}></img>
       })}
     </>
   )
